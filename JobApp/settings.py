b3609e21def125e5caa55769b226eba1b8eead5b
jell0us <magsayoangeljoy2@gmail.com>
2026-04-07 15:38:13 +0800

    added login_user in views

--- JobApp/settings.py
+++ JobApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'JobApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'job_db',
+        'USER': 'root',
+        'PASSWORD': 'rootalulab',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
--- job/views.py
+++ job/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def login_user(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        
+        return render(request, 'login.html')
+    
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Invalid password or username')
+            return redirect('login_user')
 
 # Create your views here.
